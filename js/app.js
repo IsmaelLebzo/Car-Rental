adf954b3c08d7b6bf3b87d2b67e65a844bd1c400
IsmaelLebzo <20017635@student.ltuc.com>
2021-08-31 13:03:45 +0300

    not done

--- js/app.js
+++ js/app.js
@@ -1,42 +1,75 @@
 'use strict';
-let top = ['Order Image','Order Details'];
-let containerEl = document.getElementById('container');
-let tableEl = document.getElementById('table');
-let custTable = [];
-containerEl.appendChild(tableEl);
-let formEl = document.getElementById('carForm');
-formEl.addEventListener('submit', addCar);
+let form = document.getElementById('carForm');
+let container = document.getElementById('container');
+let arrCar = [];
+let table = document.createElement('table');
+container.appendChild(table);
+table.id = 'table';
+let theadEl = document.createElement('thead');
+table.appendChild(theadEl);
+let trEl = document.createElement('tr');
+theadEl.appendChild(trEl);
+let thEl1 = document.createElement('th');
+trEl.appendChild(thEl1);
+thEl1.textContent = 'Order Image';
+let thEl2 = document.createElement('th');
+thEl2.appendChild(trEl);
+thEl2.textContent = 'Order Details';
+let tbody = document.createElement('tbody');
+table.appendChild(tbody);
 
-function carRent(custName, carModel, carPrice) {
+function saveToLocalStorage() {
+  let data = JSON.stringify(arrCar);
+  localStorage.setItem('data', data);
+}
+
+function loadFromLocalStorage() {
+  let carData = localStorage.getItem('data');
+  if (carData) {
+    let data = JSON.parse(carData);
+    for (let i = 0; i < data.length; i++) {
+      let newOrder = new CarRent(data[i].customerName, data[i].carModel, data[i].price);
+      newOrder.render();
+
+    }
+  }
+}
+
+function CarRent(custName, carModel, carPrice) {
   this.custName = custName;
   this.carModel = carModel;
-  this.carPrice = this.randCarPrice;
-  this.custTable = [];
-  this.push(this);
+  this.carPrice = carPrice;
+  arrCar.push(this);
 }
-function pushToStorage() {
-  let data = JSON.stringify(custTable);
-  localStorage.setItem('custTable', data);
-}
-function getFromStorage() {
-  custTable = JSON.parse(localStorage.getItem('custTable'));
-}
-carRent.prototype.randCarPrice = function(){
-  return Math.floor(Math.random() * (10000 - 1000 + 1)) + 1000;
+CarRent.prototype.render = function () {
+  let trEl = document.createElement('tr');
+  tbody.appendChild(trEl);
+  let tdEl = document.createElement('td');
+  tbody.appendChild(tdEl);
+  let img = document.createElement('img');
+  img.src = `img/${this.carModel}.jpg`
+  tdEl.appendChild(img);
+  let tdEl1 = document.createElement('td');
+  trEl.appendChild(tdEl1);
+  tdEl1 = `Customer Name: ${this.custName} Car Model : ${this.carModel} Car Price : ${this.carPrice}`;
+};
+loadFromLocalStorage();
+
+form.addEventListener('submit', submitHandler);
+function submitHandler(event) {
+  event.preventDefault();
+  let customerName = event.target.customerName;
+  let carModel = event.target.carModel;
+  let price = randomPrice();
+
+  let newOrder = new CarRent(customerName, carModel, price);
+  newOrder.render();
+
+  saveToLocalStorage();
+
 }
 
-function renderTHead(){
-  let theadEl = document.createElement('thead');
-  let trEl = document.createElement('tr');
-  let thEl = document.createElement('th');
-  thEl.textContent = ' ';
-  trEl.appendChild(thEl);
-  for (let i=0; i<top.length; i++){
-    let thEl = document.createElement('th');
-    thEl.textContent = top[i];
-    trEl.appendChild(thEl);
-  }
-  theadEl.appendChild(trEl);
-  tableEl.appendChild(theadEl);
+function randomPrice() {
+  return Math.floor(Math.random() * (10000 - 1000 + 1) + 1000);
 }
-renderTHead();
+
